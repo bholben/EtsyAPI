7bbee36465ad40e0ee43e445d475177f715adc5e
rdanieldesign <rdanieldesign@gmail.com>
2014-10-07 15:10:05 -0400

    Initial structure and API connection

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('The Iron Yard Rocks');
+var post_image, post_title, post_made, post_price, post_currency;
+
+etsyData.results.forEach(function(x){
+
+  post_title = x.title;
+  console.log(post_title);
+
+  post_image = x.Images.forEach(function(a){
+    a.url_fullxfull;
+  });
+
+  console.log(post_image);
+
+});
