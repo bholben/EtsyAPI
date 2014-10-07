c6b1bcdb1b53c7822eece56235d08d01f7c14902
rdanieldesign <rdanieldesign@gmail.com>
2014-10-07 19:32:41 -0400

    Style post grid

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,24 +1,25 @@
-var container = $('#post'),
+var container = $('#posts'),
 post_image, post_title, post_made, post_price, post_currency, post_item;
+
 
 etsyData.results.forEach(function(x){
 
-  x.Images.forEach(function(a){
-    post_image = '<img src="' + a.url_fullxfull + '">';
-  });
+    x.Images.forEach(function(a){
+      post_image = '<img src="' + a.url_170x135 + '">';
+    });
 
-  post_title = '<h4 class="title">' + x.title + '</h4>';
+    post_title = '<a href="#" class="title">' + x.title + '</a>';
 
-  post_made = '<span class="whoMade">' + x.Shop.shop_name + '</span>';
+    post_made = '<a href="#" class="whoMade">' + x.Shop.shop_name + '</a>';
 
-  post_currency = '<span class="currency">' + x.currency_code + '</span>';
+    post_currency = '<span class="currency">' + x.currency_code + '</span>';
 
-  post_price = '<span class="price">' + x.price + " " + post_currency + '</span>';
+    post_price = '<span class="price">' + x.price + " " + post_currency + '</span>';
 
-  post_item = '<li>' + post_image + post_title + post_made + post_price + '</li>';
+    post_icons = '<div class="icons"><a class="like" href="#"></a><a class="options" href="#"></a></div>'
 
-  console.log(post_item);
+    post_item = '<li>' + post_image + post_icons + post_title + post_made + post_price + '</li>';
 
-  container.append(post_item);
+    container.append(post_item);
 
 });
